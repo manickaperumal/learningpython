f48f00b23975eb1a6765c415f8e600a521787164
manickam2250 <manicchart@gmail.com>
2022-02-22 12:03:24 +0530

    third

--- python_programs/web_scraping/3.py
+++ python_programs/web_scraping/3.py
@@ -1,19 +1,21 @@
+from unicodedata import name
 import requests
 import os
 from bs4 import BeautifulSoup
 response=requests.get("https://en.wikipedia.org/wiki/List_of_sports")
-soup=BeautifulSoup(response.content,"html.parser")
+soup=BeautifulSoup(response.text,"html.parser")
 heading=soup.find_all("span",class_="mw-headline")
+lists=soup.find(class_="mw-headline").find_all_next("li")
+# print(lists)
 
-for i in heading:
- print(i.text)
-
-
-# types=soup.find_all("li",class_="mw-headline")
 # for i in heading:
-#  print( i.text)
+#  print(i.text)
 
-# # for j in types:
-# #    print(j)
+
+
+for i in lists:
+   print(i.a.text)
+   
+
 
 
