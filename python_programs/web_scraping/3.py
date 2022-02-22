04c3e3928756b7638607131b3061245dbd38a508
manickam2250 <manicchart@gmail.com>
2022-02-22 06:34:04 +0530

    second

--- python_programs/web_scraping/3.py
+++ python_programs/web_scraping/3.py
@@ -1,6 +1,19 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 response=requests.get("https://en.wikipedia.org/wiki/List_of_sports")
 soup=BeautifulSoup(response.content,"html.parser")
-heading=soup.find(class_="mw-headline")
-print(heading.text)
+heading=soup.find_all("span",class_="mw-headline")
+
+for i in heading:
+ print(i.text)
+
+
+# types=soup.find_all("li",class_="mw-headline")
+# for i in heading:
+#  print( i.text)
+
+# # for j in types:
+# #    print(j)
+
+
