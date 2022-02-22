e87643149bf15086afe07b2e0e0d87fba13426da
manickam2250 <manicchart@gmail.com>
2022-02-22 13:05:12 +0530

    fourth

--- python_programs/web_scraping/3.py
+++ python_programs/web_scraping/3.py
@@ -2,20 +2,19 @@ from unicodedata import name
 import requests
 import os
 from bs4 import BeautifulSoup
-response=requests.get("https://en.wikipedia.org/wiki/List_of_sports")
-soup=BeautifulSoup(response.text,"html.parser")
-heading=soup.find_all("span",class_="mw-headline")
-lists=soup.find(class_="mw-headline").find_all_next("li")
-# print(lists)
+response = requests.get("https://en.wikipedia.org/wiki/List_of_sports")
+soup = BeautifulSoup(response.text,"html.parser")
+# heading=soup.find_all("span",class_="mw-headline")
+# total_lists = soup.find(class_="mw-body-content mw-content-ltr")
 
-# for i in heading:
-#  print(i.text)
+data = dict()
 
-
-
-for i in lists:
-   print(i.a.text)
-   
+# for head in soup.find_all("h2"):
+#    if head.find("span") and head.find("span").text !="See also" and head.find("span").text != "References":
+      
+#       data['heading'] = head.find("span")
+#       print(head.parent)
 
 
 
+print(soup.find("h3").find("span", attrs={"id":"Acrobatic_sports"}).find_next("ul"))
