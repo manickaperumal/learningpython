f34aa6a745dcf4b98df3cb418f8bf557ba56fd53
manickam2250 <manicchart@gmail.com>
2022-02-23 15:33:10 +0530

    sixth

--- python_programs/web_scraping/3.py
+++ python_programs/web_scraping/3.py
@@ -1,12 +1,13 @@
 
 import requests
+import os
 from bs4 import BeautifulSoup
 response = requests.get("https://en.wikipedia.org/wiki/List_of_sports")
 soup = BeautifulSoup(response.text,"html.parser")
 # heading=soup.find_all("span",class_="mw-headline")
 # total_lists = soup.find(class_="mw-body-content mw-content-ltr")
 
-data = dict()
+# data = dict()
 
 # for head in soup.find_all("h2"):
 #    if head.find("span") and head.find("span").text !="See also" and head.find("span").text != "References":
@@ -14,25 +15,45 @@ data = dict()
 #       data['heading'] = head.find("span")
 #       print(head.parent)
 
-# def smallheading(x):
-# text=(soup.find("h3"))
-# attr=text.find("span")
 
-# print(attr["id"])
+
 
 def topic(x):
 
     for small_head in soup.find_all("h3"):
         if small_head.find("span",attrs={"id":x}):
             heading=small_head.text
+          
             sub=small_head.find_next("ul")
-            print(heading)
-            
-            print(sub.text)
+            # print(sub)
+            # print(heading)
+            path="/home/manikam"
+            os.mkdir("/home/manikam/{}".format(heading))
+            file_write_path="/home/manikam/{}".format(heading)
+            # print(file_write_path)
 
+
+            for root,dir,file in os.walk(path):
+                for i in dir:
+                    
+                    #  print (i)
+                     if i==heading:
+                        file=open("{}/1.txt".format(file_write_path),"w")
+                        file.write(str(sub.text))
+                    # else:
+                    #     # print("file")
+            
+            
+            #     file.write(sub)
+
+            
+            # print(sub.text)
+            
 
 topic("Air_sports")
 topic("Archery")
+topic("Climbing")
+topic("Cycling")
 
 
 
